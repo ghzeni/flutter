7ae19bc0b53cd2a4f3e99609ab13f7af7f4b59f9
Zeni <gustavo.zeni@pier.digital>
2024-01-21 21:31:01 -0300

    update private method

--- application/lib/main.dart
+++ application/lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
+import './questao.dart';
 
 main() {
   runApp(PerguntaApp());
 }
 
-class PerguntaAppState extends State<PerguntaApp> {
-  var perguntaSelecionada = 0;
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
 
-  void responder() {
+  void _responder() {
     setState(() {
-      perguntaSelecionada++;
+      _perguntaSelecionada++;
     });
   }
 
@@ -26,17 +27,17 @@ class PerguntaAppState extends State<PerguntaApp> {
           title: Text('Perguntas'),
         ),
         body: Column(children: <Widget>[
-          Text(perguntas[perguntaSelecionada]),
+          Questao(perguntas[_perguntaSelecionada]),
           ElevatedButton(
-            onPressed: responder,
+            onPressed: _responder,
             child: Text('Resposta 1'),
           ),
           ElevatedButton(
-            onPressed: responder,
+            onPressed: _responder,
             child: Text('Resposta 2'),
           ),
           ElevatedButton(
-            onPressed: responder,
+            onPressed: _responder,
             child: Text('Resposta 3'),
           ),
         ]),
@@ -46,7 +47,7 @@ class PerguntaAppState extends State<PerguntaApp> {
 }
 
 class PerguntaApp extends StatefulWidget {
-  PerguntaAppState createState() {
-    return PerguntaAppState();
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();
   }
 }
