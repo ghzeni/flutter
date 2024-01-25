c3a2a370d4bb85a394ffb4ac7727610a847a2547
Zeni <gustavo.zeni@pier.digital>
2024-01-25 20:16:20 -0300

    create expenses app

---
+++ expenses/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(const ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Expenses',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Versão inicial'),
+    );
+  }
+}
