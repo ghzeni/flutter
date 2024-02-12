f871e167bfe4132a69c5f982e206cf4484675997
Zeni <gustavo.zeni@pier.digital>
2024-02-12 17:41:19 -0300

    create intent testing app

---
+++ intent_testing_app/lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _a = 0;
+  int _b = 0;
+  int _sum = 0;
+
+  Future<void> _calcSum() async {
+    const channel = MethodChannel('com.example.intent_testing_playground/sum');
+
+    try {
+      final sum = await channel.invokeMethod('calcSum', {'a': _a, 'b': _b});
+      setState(() {
+        _sum = sum;
+      });
+    } on PlatformException {
+      setState(() {
+        _sum = 0;
+      });
+    }
+
+    setState(() {
+      _sum = _a + _b;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Intent testing playground'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Soma... $_sum',
+                style: TextStyle(
+                    fontSize: 30, color: Theme.of(context).colorScheme.primary),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                onChanged: (value) {
+                  setState(() {
+                    _a = int.tryParse(value) ?? 0;
+                  });
+                },
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                onChanged: (value) {
+                  setState(() {
+                    _b = int.tryParse(value) ?? 0;
+                  });
+                },
+              ),
+              ElevatedButton(onPressed: _calcSum, child: Text('somar')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  /* _openAudioRecorder() {
+    // open the pop-up to record audio
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Container();
+      },
+    );
+  } */
+}
